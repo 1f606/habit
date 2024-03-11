e9f1cead5f05141821786a4a38f07472b07072cc
周艳凯 <750419898@qq.com>
2024-03-11 13:15:16 +0800

    feat(添加loading hooks): 加loading hooks
    加loading hooks

--- src/pages/tabbar/index.vue
+++ src/pages/tabbar/index.vue
@@ -1,40 +1,40 @@
 <template>
-    <div>
-        <home v-if="tabbar === 'home'"></home>
-        <wode v-if="tabbar === 'wode'"></wode>
-        <poput v-if="tabbar === 'poput'"></poput>
-        <wd-tabbar
-            v-model="tabbar"
-            fixed
-            bordered
-            safe-area-inset-bottom
-            shape="round"
-        >
-            <wd-tabbar-item
-                title="首页"
-                is-dot
-                name="home"
-                :value="1"
-                icon="home"
-            ></wd-tabbar-item>
-            <wd-tabbar-item
-                title="分类"
-                :value="2"
-                name="wode"
-                icon="cart"
-            ></wd-tabbar-item>
-            <wd-tabbar-item
-                title="相册"
-                :value="3"
-                name="poput"
-                icon="photo"
-            ></wd-tabbar-item>
-            <wd-tabbar-item
-                title="我的"
-                :value="4"
-                icon="user"
-            ></wd-tabbar-item>
-        </wd-tabbar>
+    <div class="contain h-full flex flex-col">
+        <div class="content flex-1">
+            <home v-if="tabbar === 'home'"></home>
+            <wode v-if="tabbar === 'wode'"></wode>
+            <poput v-if="tabbar === 'poput'"></poput>
+            <user v-if="tabbar === 'user'"></user>
+        </div>
+        <div class="tabbar">
+            <wd-tabbar v-model="tabbar" fixed bordered safe-area-inset-bottom>
+                <wd-tabbar-item
+                    title="首页"
+                    is-dot
+                    name="home"
+                    :value="1"
+                    icon="home"
+                ></wd-tabbar-item>
+                <wd-tabbar-item
+                    title="分类"
+                    :value="2"
+                    name="wode"
+                    icon="cart"
+                ></wd-tabbar-item>
+                <wd-tabbar-item
+                    title="相册"
+                    :value="3"
+                    name="poput"
+                    icon="photo"
+                ></wd-tabbar-item>
+                <wd-tabbar-item
+                    title="我的"
+                    :value="4"
+                    name="user"
+                    icon="user"
+                ></wd-tabbar-item>
+            </wd-tabbar>
+        </div>
     </div>
 </template>
 <script lang="ts" setup>
@@ -42,6 +42,7 @@ import { onMounted, ref } from 'vue';
 import home from '../index/index.vue'; //首页
 import wode from '../wode/index.vue'; //首页
 import poput from '../poput/index.vue';
+import user from '../user/index.vue'; //我的页面
 import { useRoute } from 'uni-mini-router';
 const router = useRoute();
 const tabbar = ref('home');
@@ -58,5 +59,10 @@ onMounted(() => {
     .wd-badge__content {
         border: none !important;
     }
+}
+
+.tabbar {
+    margin-bottom: env(safe-area-inset-bottom);
+    height: var(--wot-tabbar-height, 50px);
 }
 </style>
